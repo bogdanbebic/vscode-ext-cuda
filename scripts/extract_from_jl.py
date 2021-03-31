04d33407161a6ea7049be4ff9771d4e29f6e8bda
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-03-31 17:20:49 +0200

    scripts/extract_from_jl.py: Implement split data to multiple json files

--- scripts/extract_from_jl.py
+++ scripts/extract_from_jl.py
@@ -1,5 +1,31 @@
 import argparse
+import json
 import pathlib
+
+
+def extract_functions_data(json_obj):
+    # TODO: implement
+    return json_obj
+
+
+def extract_defines_data(json_obj):
+    # TODO: implement
+    return json_obj
+
+
+def extract_typedefs_data(json_obj):
+    # TODO: implement
+    return json_obj
+
+
+def extract_enumerations_data(json_obj):
+    # TODO: implement
+    return json_obj
+
+
+def extract_enum_members_data(json_obj):
+    # TODO: implement
+    return json_obj
 
 
 def main():
@@ -37,7 +63,37 @@ def main():
             f"Invalid DIRECTORY argument, not a valid directory: {args.target_directory}"
         )
 
-    # TODO: implement
+    # Extract data from source json lines file
+    functions_json = []
+    defines_json = []
+    typedefs_json = []
+    enumerations_json = []
+    enum_members_json = []
+    with open(args.source) as source_file:
+        for line in source_file:
+            json_obj = json.loads(line)
+            if json_obj["kind"] == "function":
+                functions_json.append(extract_functions_data(json_obj))
+            elif json_obj["kind"] == "define":
+                defines_json.append(extract_defines_data(json_obj))
+            elif json_obj["kind"] == "typedef":
+                typedefs_json.append(extract_typedefs_data(json_obj))
+            elif json_obj["kind"] == "enum":
+                enumerations_json.append(extract_enumerations_data(json_obj))
+            elif json_obj["kind"] == "enum-member":
+                enum_members_json.append(extract_enum_members_data(json_obj))
+
+    # Write extracted data to output files
+    with open(f"{args.target_directory}/functions.json", "w") as functions_file:
+        json.dump(functions_json, functions_file, indent=4)
+    with open(f"{args.target_directory}/defines.json", "w") as defines_file:
+        json.dump(defines_json, defines_file, indent=4)
+    with open(f"{args.target_directory}/typedefs.json", "w") as typedefs_file:
+        json.dump(typedefs_json, typedefs_file, indent=4)
+    with open(f"{args.target_directory}/enumerations.json", "w") as enumerations_file:
+        json.dump(enumerations_json, enumerations_file, indent=4)
+    with open(f"{args.target_directory}/enum_members.json", "w") as enum_members_file:
+        json.dump(enum_members_json, enum_members_file, indent=4)
 
 
 if __name__ == "__main__":
