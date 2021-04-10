48ee5bd30f5e9299995dd3ac27cf664ca2cc04fc
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-04-10 19:18:53 +0200

    extension, .gitignore: Move resource files to own folder

--- src/extension.ts
+++ src/extension.ts
@@ -3,19 +3,19 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	const defaultTriggerChars = '_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-	let functions = require('../spider_output/functions.json');
+	let functions = require('../resources/functions.json');
 	functions.forEach((element: vscode.CompletionItem) => element.kind = vscode.CompletionItemKind.Function);
 
-	let defines = require('../spider_output/defines.json');
+	let defines = require('../resources/defines.json');
 	defines.forEach((element: vscode.CompletionItem) => element.kind = vscode.CompletionItemKind.Constant);
 
-	let typedefs = require('../spider_output/typedefs.json');
+	let typedefs = require('../resources/typedefs.json');
 	typedefs.forEach((element: vscode.CompletionItem) => element.kind = vscode.CompletionItemKind.Interface);
 
-	let enumerations = require('../spider_output/enumerations.json');
+	let enumerations = require('../resources/enumerations.json');
 	enumerations.forEach((element: vscode.CompletionItem) => element.kind = vscode.CompletionItemKind.Enum);
 
-	let enumMembers = require('../spider_output/enum_members.json');
+	let enumMembers = require('../resources/enum_members.json');
 	enumMembers.forEach((element: vscode.CompletionItem) => element.kind = vscode.CompletionItemKind.EnumMember);
 
 	let allItems = functions.concat(defines, typedefs, enumerations, enumMembers);
